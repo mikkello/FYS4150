acdd5c1f1039b86a2869071cbb3f43be2eadaf6c
mikkello <mikkellokc@gmail.com>
2017-09-04 15:46:28 +0200

    Tests

---
+++ hello world/hello_world/main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include <qDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug() <<  "hello world";
+
+    return a.exec();
+}
